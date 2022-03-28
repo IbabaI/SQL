1c6109aa3cacc387df9736f687631ad04eaa4805
707BIGBOSS\user <2001mist@naver.com>
2022-03-28 23:25:21 +0900

    기본사칙연산공부

---
+++ SQL/기본사칙연산공부.sql
@@ -0,0 +1,18 @@
+select empno, ename from emp;
+
+-- 사칙연산
+select sal + 300 from emp;  -- sal 값에 +300
+select sal - 300 from emp;
+select sal * 300 from emp;
+select round(sal / 300) from emp;
+
+select ename, sal, sal*12 from emp;
+
+select ename, sal, job, deptno, comm from emp;
+
+select ename, sal, job, deptno, comm, sal*12, sal*12+comm from emp;
+
+select ename, sal, job, deptno, nvl(comm,0), sal*12, sal*12+nvl(comm,0) "연 봉" from emp;
+
+--날짜
+select sysdate from dual;
